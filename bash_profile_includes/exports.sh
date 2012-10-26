979ab33faa3cc9f119aa585d61a45a7210408c39
Dimitri Roche <dimroc@gmail.com>
2012-10-26 07:54:48 -0400

    EDITOR and VISUAL exports are vim/mvim

--- bash_profile_includes/exports.sh
+++ bash_profile_includes/exports.sh
@@ -6,7 +6,8 @@ export GREP_OPTIONS='--color=auto'
 export LSCOLORS=Exfxcxdxbxegedabagacad
 # export LSCOLORS=gxfxcxdxbxegedabagacad # Dark lscolor scheme
 export HISTCONTROL=ignoredups
-export EDITOR=mvim
+export EDITOR=vim
+export VISUAL=mvim
 
 # PATH
 export PATH=./node_modules/.bin:$PATH

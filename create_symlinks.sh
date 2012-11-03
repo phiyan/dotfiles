9cf28eb4a3bd4c0420e4f6bdb968ff60146b8da5
Dimitri Roche <dimroc@gmail.com>
2012-11-03 17:21:13 -0400

    bash script to create symlink to dot file

---
+++ create_symlinks.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+dotfiles=( ackrc bash_profile bash_profile_includes gitconfig inputrc irbrc rdebugrc gdbinit )
+source=$1
+
+if [ -z $source ]; then
+  echo "Must pass the directory where the dotfiles live"
+  exit 1
+fi
+
+for dotfile in "${dotfiles[@]}"; do
+  rm -f ".${dotfile}"
+  ln -s "${source}/${dotfile}" ".${dotfile}"
+done

dfa41e374310daacb195ee52e2d13dc7717f6d9b
Dimitri Roche <dimroc@gmail.com>
2013-04-30 09:32:22 -0400

    Add AWS_RDS_HOME env var

--- bash_profile_includes/infogroup.sh
+++ bash_profile_includes/infogroup.sh
@@ -5,6 +5,7 @@ export ARCHFLAGS="-arch x86_64"
 
 export JAVA_HOME="$(/usr/libexec/java_home)"
 export EC2_HOME="/usr/local/Library/LinkedKegs/ec2-api-tools/jars"
+export AWS_RDS_HOME="/usr/local/Cellar/rds-command-line-tools/1.10.003/libexec"
 
 alias gqa="source ~/.aws/accounts/gotime_qa/rc"
 #alias gprod="source ~/.aws/accounts/gotime_prod/rc"

5331e4856f59d6a3bece5fb7f62b2c9ba0252e56
Dimitri Roche <dimroc@gmail.com>
2014-02-06 15:35:22 -0500

    Add alias rtest

--- bash_profile_includes/aliases.sh
+++ bash_profile_includes/aliases.sh
@@ -27,3 +27,4 @@ alias gitchanges="git st -s | sed 's/^...//g'"
 alias gitchangesspec='git st -s | cut -c 4- | ack spec | ack -v ".*(stubs|factories).*"'
 alias ag="ag -i"
 alias tmigrate="rake db:migrate db:rollback && rake db:migrate db:test:prepare"
+alias rtest="ruby -Itest"
